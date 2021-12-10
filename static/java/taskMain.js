511a9dc1da947a0aa822ff84d12aea2bfe0f732a
chenchao <chenchao@dataqiao.cn>
2021-12-10 16:04:15 +0800

    修改生产文件

--- static/java/taskMain.js
+++ static/java/taskMain.js
@@ -2,19 +2,16 @@
 const fs = require('fs')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
-
 const b = process.env.NODE_ENV === 'production'
-
 const u1 = b ? path.join(__dirname, '..', '..', 'app', 'node_modules', 'electron-re') : 'electron-re'
 const u2 = b ? path.join(__dirname, '..', '..', 'app', 'node_modules', 'java') : 'java'
 // const re = require('C:\\Program Files\\cc_data_link\\resources\\app\\node_modules\\electron-re')//dynamicallyRequire('electron-re')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const re = require(u1)
-const ProcessHost = re.ProcessHost
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const java = require(u2)
-
-const baseDir = b ? path.join('C:\\Program Files\\DLTOpenJDK\\', 'dlt_db') : path.join(__dirname, '..', '..', 'static', 'dlt_db').replace(/\\/g, '\\\\')
+const ProcessHost = re.ProcessHost
+const baseDir = b ? 'C:\\Program Files\\DLTOpenJDK\\dlt_db' : path.join(__dirname, '..', '..', 'static', 'dlt_db').replace(/\\/g, '\\\\')
 
 const dependencies = fs.readdirSync(baseDir)
 dependencies.forEach(function (dependency) {
