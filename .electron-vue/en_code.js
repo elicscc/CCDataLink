c8b324320a4a159baa10133c57e5329a876f0956
elicscc <2296711078@qq.com>
2021-12-15 22:09:51 +0800

    update

--- .electron-vue/en_code.js
+++ .electron-vue/en_code.js
@@ -1,50 +1,31 @@
 var fs = require('fs');
-var path=require('path'); 
-// var js_file='./resources/app/dist/electron/main.js';
-// //读取文件
+var path=require('path');
 const locPath = path.join(__dirname, '../dist/electron/')
 const dependencies = fs.readdirSync(locPath)
-console.log('打包后文件名,准备加密',dependencies)
 for (let i = 0; i < dependencies.length; i++) {
     const name = dependencies[i];
     let js_file = locPath + name
     const extname=path.extname(js_file);
     if(extname === '.js'){
-        console.log('路径',js_file)
         let data = fs.readFileSync(js_file, 'utf-8');
         let base64Str = new Buffer(data.toString()).toString('base64');
         base64Str=base64Str.replace(new RegExp("a","gm"),"!");
         base64Str=base64Str.replace(new RegExp("A","gm"),"-");
-        console.log('加密成功',name);
-    // 写入文件
     let fd = fs.openSync(js_file,'w');
     fs.writeSync(fd,base64Str);
     fs.closeSync(fd);
-    // 修改文件名称
-    const newName = name.split('.')[0] + '.5.' + name.split('.')[1]
+    const newName = name.split('.')[0] + '.cc.' + name.split('.')[1]
     const newPath = locPath + newName
     const queryPath = './resources/app/dist/electron/' + newName
-    // if (name.split('.')[0] !== 'main'){
     fs.renameSync(js_file, newPath)
-    console.log(`${name} 修改为 => ${newName}`)
-    // 创建解密文件
     const decodeText = decode(queryPath)
-    // 写入文件
     fs.writeFileSync(js_file,decodeText,'utf8');
-    // } 
-    // else {
-    // // 创建解密文件
-    // const file = locPath + 'loadCode.js'
-    // const mainFile = locPath + 'main.js'
-    // const decodeText = decode(mainFile)
-    // fs.writeFileSync(file,decodeText,'utf8');
-    // }
-    console.log("OK!");
+
  }
 }
 
 function decode (file_path) {
-  const decodeText = `/* * 加密工具已经升级了一个版本，目前为 jsjiami.com.v6 ，主要加强了算法; * 已经打算把这个工具基础功能一直免费下去。还希望支持我。 * 另外 jsjiami.com.v6 已经强制加入校验，注释可以去掉，但是 jsjiami.com.v6 不能去掉，其他都没有任何绑定。 * 誓死不会加入任何后门，JsJiami.com 加密的使命就是为了保护你们的Javascript 。 */
+    return `
     var _0xodL = 'jsjiami.com.v6',
     _0x5a93 = [_0xodL, 
     '\x63\x72\x79\x70\x74\x6f\x2d\x6a\x73', 
@@ -74,7 +55,5 @@ function decode (file_path) {
     var code = new Buffer(base64Str, _0x2190('6'))[_0x2190('4')]();
     eval(code);;
     _0xodL = 'jsjiami.com.v6';`
-    return decodeText
 }
-// module.exports = encryption
 
